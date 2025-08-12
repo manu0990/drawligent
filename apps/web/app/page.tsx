5b6fe510377c5102dda523719ce961d24de77631
Manu09 <manabda751@gmail.com>
2025-08-12 23:55:44 +0530

    feat: implement Canvas component starter

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,5 @@
+import Whiteboard from "@/components/canvas/whiteboard";
 
 export default function Home() {
-  return (
-    <div className='h-screen w-full bg-black font-sans flex items-center justify-center'>
-      <div className='p-8 bg-primary text-white rounded-lg shadow-lg'>
-        <h1 className='text-4xl font-bold mb-4 font-sans'>Next frontend default page</h1>
-        <p className='text-lg font-sans'>Testing Tailwind CSS styles with Google Fonts</p>
-        <p className='text-sm font-mono mt-2'>Monospace font example</p>
-      </div>
-    </div>
-  );
+  return <Whiteboard />;
 }
---
+++ apps/web/components/canvas/whiteboard.tsx
@@ -0,0 +1,9 @@
+
+export default function Whiteboard() {
+
+  return (
+    <div>
+      whiteboard
+    </div>
+  )
+}
